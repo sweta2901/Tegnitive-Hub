6c4c621a0b3ddff5a65c53ae619e6c7780fd5b93
sweta2901 <88038051+sweta2901@users.noreply.github.com>
2023-04-04 10:35:38 +0530

    changed

--- TegnitiveHub/TegnitiveHub/settings.py
+++ TegnitiveHub/TegnitiveHub/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin',
+    'faculty',
     'import_export',
 ]
 
--- TegnitiveHub/TegnitiveHub/urls.py
+++ TegnitiveHub/TegnitiveHub/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
-from myapp import views
+from faculty import views
 
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('admin.urls')),
+    path('', include('faculty.urls')),
     path('',views.home,name="home"),
 ]
