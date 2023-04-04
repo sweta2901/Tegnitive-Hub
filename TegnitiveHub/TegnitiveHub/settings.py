f40087cdd100b7dcf48a9153b0c52b40a255a628
sweta2901 <88038051+sweta2901@users.noreply.github.com>
2023-04-04 10:53:01 +0530

    changed the path

--- TegnitiveHub/TegnitiveHub/settings.py
+++ TegnitiveHub/TegnitiveHub/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'faculty/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "faculty/static/"),
 )
 
 # Default primary key field type
